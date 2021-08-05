63e2453e4150366777c496bbe0a866472030af17
Amit Biswas <biswas1992@hotmail.com>
2021-08-04 23:59:20 -0400

    14. Longest Common Prefix

---
+++ Problems-Categories/Strings/14. Longest Common Prefix.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+
+    func longestCommonPrefix(_ strs: [String]) -> String {
+        if strs.count == 0 { return "" }
+       
+        var minStr = (strs.min{$0.count < $1.count})!
+        
+        for str in strs{
+            while !str.hasPrefix(minStr){
+                 minStr.removeLast()
+            }
+        }
+        
+        return minStr
+    }
+
---
+++ Problems-Categories/Strings/3. Longest Substring Without Repeating Characters.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// MARK:- 3. Longest Substring Without Repeating Characters
+
+
+func lengthOfLongestSubstring(_ s: String) -> Int {
+    if s == "" || s.count == nil {
+        return 0
+    }
+    var i = 0 , j = 0 , max = 0
+    var hashSet: [Int : String ] = [:]
+    
+    while i < s.count  {
+        var c = 
+    }
+    
+
+
+return 0
+}
