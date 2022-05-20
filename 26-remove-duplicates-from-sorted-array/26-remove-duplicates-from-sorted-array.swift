89516ba79a3e772b5473cd0cae02ab86c789c66a
Amit  Biswas <biswas1992@hotmail.com>
2022-05-19 23:16:30 -0400

    Time: 61 ms (61.23%), Space: 14.8 MB (55.84%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.swift
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.swift
@@ -1,15 +1,16 @@
 class Solution {
     func removeDuplicates(_ nums: inout [Int]) -> Int {
-     
-        var l = 1 
+        // [1,1,3,4,5,5,6]
         
-        for r in 1..<nums.count {
-            if nums[r] != nums[r - 1] {
-                nums[l] = nums[r] 
-                
-                l += 1 
+        var leftPointer = 1 
+        
+        for rightPointer in 1..<nums.count {
+            if nums[rightPointer] != nums[rightPointer - 1] {
+                nums[leftPointer] = nums[rightPointer] 
+                leftPointer += 1
             }
         }
-        return l
-    }
+        
+        return leftPointer
+}
 }
